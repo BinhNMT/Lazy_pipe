dd48fdb3122b43065af17dfa93c2491264d930e8
BinhTMNguyen <33645619+BinhTMNguyen@users.noreply.github.com>
2019-08-20 21:26:47 +0700

    Add files via upload
    The first commit

---
+++ Application Files/Release_V3.0/DoAnTotNghiepV3.0/Form1.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Cắt giao tuyến", "Cắt đoạn"})
+        Me.ComboBox1.Location = New System.Drawing.Point(78, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Lime
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button1.Location = New System.Drawing.Point(105, 240)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(73, 62)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(78, 85)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(78, 120)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(78, 159)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 4
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(78, 199)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 91)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(28, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "D(A)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(28, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "D(B)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 165)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Alpha"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 205)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(22, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "I(a)"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(185, 91)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "(mm)"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(185, 126)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(29, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "(mm)"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(185, 165)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(33, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "(Deg)"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(184, 206)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(29, 13)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "(mm)"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 15)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(60, 13)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "Chế độ cắt"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Ống A", "Ống B"})
+        Me.ComboBox2.Location = New System.Drawing.Point(78, 48)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox2.TabIndex = 15
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button2.Location = New System.Drawing.Point(15, 240)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(73, 61)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "RESET"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 53)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(53, 13)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "Chọn ống"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(261, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(343, 290)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 19
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.ClientSize = New System.Drawing.Size(616, 314)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "LazyPipeV3.4"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Application Files/Release_V3.0/DoAnTotNghiepV3.0/Form1.vb
@@ -0,0 +1,159 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim tutu As StreamWriter
+        tutu = New StreamWriter("C:\Users\Lenovo\Desktop\LazyPipeGcode.txt")
+
+        Dim dkA, dkB, R As Single                   ' đường kính, bán kính
+        Dim y, ax, bx, beginA, beginY As Single     ' các biến của PT bậc 2
+        Dim delta, a, b, c, d As Single             ' các biến của PT đường tròn
+        Dim x1, xA, xB, xC As Single                ' tọa độ điểm thứ 2 cắt đoạn
+        Dim x2, yA, yB, yC As Single                ' tọa độ điểm thứ 3 cắt đoạn
+        Dim PI, alpha, Adb, rad, luongiac, luonggiac As Single
+        Dim h, k, heso As Single
+        'Dim Axis As Single              ' Trục A
+
+        dkA = Val(TextBox1.Text)
+        dkB = Val(TextBox2.Text)
+        alpha = Val(TextBox3.Text)
+        a = Val(TextBox4.Text)
+
+        Adb = 180 * (dkB / dkA)                                 ' góc quay tương ứng đường kính ống B
+        PI = 3.14159265
+        rad = PI / 180                                          ' chuyển đổi radian
+        luongiac = Math.Sin(alpha * rad)                        ' sin alpha
+        R = Adb / 2                                             ' bán kính R
+        b = 180 / 2                                             ' I(a,b)
+        c = Math.Pow(a, 2) + Math.Pow(b, 2) - Math.Pow(R, 2)    ' c trong PTĐT
+        beginA = (180 - Adb) / 2                                ' điểm bắt đầu cắt giao tuyến theo độ
+        'beginY = (beginA * dkA) / 180                           ' điểm bắt đầu cắt giao tuyến theo Y
+
+        'heso = (dkB / Adb)                                      ' tỷ số mm/độ => 1 độ ứng với số mm
+        'h = y + heso
+        'k = y - heso
+        '--
+        luonggiac = Math.Tan(alpha * rad) ' tan alpha
+        xA = a                            ' tọa độ bắt đầu cắt đoạn
+        xB = dkA / luonggiac              ' tọa độ thứ 2 cắt đoạn
+        yB = dkA
+        xC = xA                           ' tọa độ thứ 3 cắt đoạn
+        yA = 0
+        yC = dkA * 2
+        '--
+        tutu.WriteLine("G0 " + "X0" + " Y0" + " Z-5")
+        tutu.WriteLine("G0 " + "F1000")
+
+        If ComboBox1.SelectedItem = "Cắt giao tuyến" Then
+
+            If ComboBox2.SelectedItem = "Ống A" Then
+
+                For y = beginA To Adb + beginA Step +1
+
+                    d = Math.Pow(y, 2) - (2 * b * y) + c
+                    ax = 1
+                    bx = -2 * a
+                    'Axis = (y * Adb) / dkA
+                    delta = Math.Pow(bx, 2) - (4 * ax * d)
+
+                    If delta = 0 Then
+                        x1 = x2 = ((-bx) / (2 * ax)) / luongiac
+                        tutu.WriteLine("G1 " + "X" & x1.ToString() + " Y" & y.ToString() + " Z0")
+                        'tutu.WriteLine("G1 " + "X" & x1.ToString() + " A" & Axis.ToString())
+
+                    ElseIf delta > 0 Then
+                        x1 = (((-bx) + Math.Sqrt(delta)) / (2 * ax)) / luongiac
+                        x2 = (((-bx) - Math.Sqrt(delta)) / (2 * ax)) / luongiac
+                        tutu.WriteLine("G1 " + "X" & x1.ToString() + " Y" & y.ToString() + " Z0")
+                        'tutu.WriteLine("G1 " + "X" & x1.ToString() + " A" & Axis.ToString())
+
+                    End If
+
+                Next y
+
+                For y = Adb + beginA To beginA Step -1
+
+                    d = Math.Pow(y, 2) - (2 * b * y) + c
+                    ax = 1
+                    bx = -2 * a
+                    'Axis = (y * Adb) / dkA
+                    delta = Math.Pow(bx, 2) - (4 * ax * d)
+
+                    If delta = 0 Then
+                        x1 = x2 = ((-bx) / (2 * ax)) / luongiac
+                        tutu.WriteLine("G1 " + "X" & x2.ToString() + " Y" & y.ToString() + " Z0")
+                        'tutu.WriteLine("G1 " + "X" & x2.ToString() + " A" & Axis.ToString())
+
+                    ElseIf delta > 0 Then
+                        x1 = (((-bx) + Math.Sqrt(delta)) / (2 * ax)) / luongiac
+                        x2 = (((-bx) - Math.Sqrt(delta)) / (2 * ax)) / luongiac
+                        tutu.WriteLine("G1 " + "X" & x2.ToString() + " Y" & y.ToString() + " Z0")
+                        'tutu.WriteLine("G1 " + "X" & x2.ToString() + " A" & Axis.ToString())
+
+                    End If
+
+                Next y
+
+
+
+            ElseIf ComboBox2.SelectedItem = "Ống B" Then
+
+                For y = 0 To dkA Step h
+
+                Next y
+                '--
+                For y = dkA To dkA * 2 Step h
+
+                Next y
+
+            Else
+                MessageBox.Show("Xin chọn ống")
+
+            End If
+
+
+            '---------------------------------------------------------------------------------------
+        ElseIf ComboBox1.SelectedItem = "Cắt đoạn" Then
+
+            For y = yA To yB
+
+                'Axis = (y * Adb) / dkB
+
+                x1 = (-y * (xB - xA) - (yB * xA) + (yA * xB)) / (yA - yB)
+                tutu.WriteLine("G1 " + "X" & x1.ToString() + " Y" & y.ToString() + " Z0")
+                'tutu.WriteLine("G1 " + "X" & x1.ToString() + " A" & Axis.ToString())
+
+            Next y
+
+            For y = yB To yC
+
+                'Axis = (y * Adb) / dkB
+
+                x1 = (-y * (xC - xB) - (yC * xB) + (yB * xC)) / (yB - yC)
+                tutu.WriteLine("G1 " + "X" & x1.ToString() + " Y" & y.ToString() + " Z0")
+                'tutu.WriteLine("G1 " + "X" & x1.ToString() + " A" & Axis.ToString())
+
+            Next y
+
+            '---------------------------------------------------------------------------------------
+        Else
+            MessageBox.Show("Xin chọn Chế độ cắt")
+
+        End If
+
+        tutu.WriteLine("G0 Z-5")
+        tutu.WriteLine("G28")
+        tutu.WriteLine("M2")
+        tutu.Close()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+
+    End Sub
+End Class
